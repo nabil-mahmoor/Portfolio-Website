2735f5ed930ac0d234e9d8ca69e9b141da9fc9d8
Nabil <nabil.mahmoor@gmail.com>
2024-12-10 00:15:18 +0530

    First commit

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    if (empty($name) || empty($mailFrom) || empty($message)) {
+        header("Location: index.html?form=empty&name=" . $name . "&email=" . $mailFrom);
+        exit();
+    } else if (!filter_var($mailFrom, FILTER_VALIDATE_EMAIL)) {
+        exit();
+        header("Location: index.html?form=invalid_email&name=" . $name);
+    } else if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+        header("Location: index.html?form=invalid_name&email=" . $email);
+        exit();
+    }
+
+    $mailTo = "nabil.mahmoor@gmail.com";
+    $subject = "You have received an email from " . $name;
+    $headers = "From: " . $mailFrom;
+
+    mail($mailTo, $subject, $message, $headers);
+} else {
+    header("Location: index.html?form=error");
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+const mobileNav = document.getElementById("mobile-navigation");
+
+function openMenu() {
+    mobileNav.classList.remove('hide');
+}
+
+function closeMenu() {
+    mobileNav.classList.add('hide');
+}
