de90c728449241421e0b12a852a2a429e5695473
Nabil <nabil.mahmoor@gmail.com>
2024-12-10 12:40:22 +0530

    Fixed form to redirect to contact section

--- form.php
+++ form.php
@@ -6,21 +6,22 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
 
     if (empty($name) || empty($mailFrom) || empty($message)) {
-        header("Location: index.html?form=empty&name=" . $name . "&email=" . $mailFrom);
+        header("Location: index.html?form=empty&name=" . $name . "&email=" . $mailFrom . "#contact");
         exit();
     } else if (!filter_var($mailFrom, FILTER_VALIDATE_EMAIL)) {
         exit();
-        header("Location: index.html?form=invalid_email&name=" . $name);
+        header("Location: index.html?form=invalid_email&name=" . $name . "#contact");
     } else if (!preg_match("/^[a-zA-Z]*$/", $name)) {
-        header("Location: index.html?form=invalid_name&email=" . $email);
+        header("Location: index.html?form=invalid_name&email=" . $email . "#contact");
         exit();
+    } else {
+        $mailTo = "nabil.mahmoor@gmail.com";
+        $subject = "You have received an email from " . $name;
+        $headers = "From: " . $mailFrom;
+    
+        mail($mailTo, $subject, $message, $headers);
     }
 
-    $mailTo = "nabil.mahmoor@gmail.com";
-    $subject = "You have received an email from " . $name;
-    $headers = "From: " . $mailFrom;
-
-    mail($mailTo, $subject, $message, $headers);
 } else {
-    header("Location: index.html?form=error");
+    header("Location: index.html?form=error#contact");
 }
